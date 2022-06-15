52110bde9b53433fa7fabc8be16bacdebacbdbd0
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-06-15 14:55:36 -0300

    feat: back-end submit feedback

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "bfb82d956012f1",
+    pass: "71b1b8db27752b",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Lucas Matos <lucasprzm@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
